f6d9b996d297d687271239f63c1ca044f67bdc75
Confidence Okoghenun <cokoghenun@gmail.com>
2023-09-13 08:42:44 +0000

    feat: Adds persistent storage

--- pages/Page1/jsobjects/app/app.js
+++ pages/Page1/jsobjects/app/app.js
@@ -1,9 +1,10 @@
 export default {
 	init () {
-		data.todos = data.dummyTodos;
+		data.todos = appsmith.store.todos || data.dummyTodos;
 	},
 	updateTodo (id, update) {
 		data.todos[id] = {...data.todos[id], ...update};
+		storeValue('todos', data.todos);
 	},
 	addTodo (title) {
 		this.updateTodo(data.todos.length, {title, completed: false});
