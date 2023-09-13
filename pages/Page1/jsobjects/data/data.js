77a7e508b127f5fd7d145cbbdbec14fab9fb478b
Confidence Okoghenun <cokoghenun@gmail.com>
2023-09-13 08:21:26 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/data/data.js
@@ -0,0 +1,8 @@
+export default {
+	todos: undefined,
+	dummyTodos: [
+		{title: 'use the input to add a todo', completed: true},
+		{title: 'hit enter to submit', completed: false},
+		{title: 'click on a todo to mark as done', completed: false},
+		{title: 'have fun', completed: false,}],
+}
