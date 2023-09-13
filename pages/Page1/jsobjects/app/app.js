590273da9866c387287ea6f5609172d1e8d2269a
Confidence Okoghenun <cokoghenun@gmail.com>
2023-09-13 08:27:52 +0000

    feat: Adds add todo flow

--- pages/Page1/jsobjects/app/app.js
+++ pages/Page1/jsobjects/app/app.js
@@ -2,4 +2,10 @@ export default {
 	init () {
 		data.todos = data.dummyTodos;
 	},
+	updateTodo (id, update) {
+		data.todos[id] = {...data.todos[id], ...update};
+	},
+	addTodo (title) {
+		this.updateTodo(data.todos.length, {title, completed: false});
+	},
 }
